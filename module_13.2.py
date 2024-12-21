ae85c9f29d65f6c2afad53e32634c47edf8b9eee
ramilmavlihanov <Ramil040201@gmail.com>
2024-12-21 18:43:13 +0300

    module_13_2 done

---
+++ module_13.2.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import ApiKey
+
+api = ApiKey.apiKey
+bot = Bot(token=api)
+dispatcher = Dispatcher(bot=bot, storage=MemoryStorage())
+
+@dispatcher.message_handler(commands="start")
+async def start(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+@dispatcher.message_handler()
+async def all_message(message):
+    print("Введите команду /start, чтобы начать общение.'")
+
+if __name__ == "__main__":
+    executor.start_polling(dispatcher, skip_updates=True)
